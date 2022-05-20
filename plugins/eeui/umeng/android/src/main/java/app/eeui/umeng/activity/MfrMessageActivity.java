be40be7239834aa055621aa8054c9376e6a563a3
kuaifan <aipaw@live.cn>
2022-05-20 11:39:08 +0800

    更新小米华为推送

---
+++ plugins/eeui/umeng/android/src/main/java/app/eeui/umeng/activity/MfrMessageActivity.java
@@ -0,0 +1,19 @@
+package app.eeui.umeng.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.umeng.message.UmengNotifyClickActivity;
+
+public class MfrMessageActivity extends UmengNotifyClickActivity {
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+    }
+
+    @Override
+    public void onMessage(Intent intent) {
+        super.onMessage(intent);
+    }
+}
