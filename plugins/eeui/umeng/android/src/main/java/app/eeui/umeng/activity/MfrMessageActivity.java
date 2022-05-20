47eec579b115d366848d777bee7106ad09e14c08
kuaifan <aipaw@live.cn>
2022-05-20 16:42:54 +0800

    perf: 优化离线推送

--- plugins/eeui/umeng/android/src/main/java/app/eeui/umeng/activity/MfrMessageActivity.java
+++ plugins/eeui/umeng/android/src/main/java/app/eeui/umeng/activity/MfrMessageActivity.java
@@ -2,14 +2,21 @@ package app.eeui.umeng.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Looper;
 
 import com.umeng.message.UmengNotifyClickActivity;
+
+import app.eeui.framework.ui.eeui;
 
 public class MfrMessageActivity extends UmengNotifyClickActivity {
 
     @Override
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
+        eeui myEeui = new eeui();
+        String appId = eeui.getApplication().getPackageName();
+        myEeui.openOtherAppTo(this, appId, appId + ".WelcomeActivity", null);
+        new android.os.Handler(Looper.getMainLooper()).postDelayed(this::finish, 1000);
     }
 
     @Override
