6b9e8cdd5d3a928707c5f24941a14fcdd03f539d
kuaifan <aipaw@live.cn>
2022-05-31 17:37:06 +0800

    no message

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -55,8 +55,8 @@ export default {
         }
         //
         eeui.setStatusBarStyle(false)
-        this.$refs.web.setUrl("http://192.168.0.111:2222");
-        this.$refs.web.setUrl("http://192.168.200.150:2222");
+        // this.$refs.web.setUrl("http://192.168.0.111:2222");
+        // this.$refs.web.setUrl("http://192.168.200.150:2222");
         this.$refs.web.setUrl(eeui.rewriteUrl('../public/index.html'));
     },
 
