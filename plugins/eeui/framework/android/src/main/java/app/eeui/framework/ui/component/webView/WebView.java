5d4a00d05030b3c20b0db4ea4bef3a3b54b3fe45
kuaifan <aipaw@live.cn>
2025-04-09 08:50:40 +0800

    feat: Android webview 组件添加 fullscreen 参数

--- plugins/eeui/framework/android/src/main/java/app/eeui/framework/ui/component/webView/WebView.java
+++ plugins/eeui/framework/android/src/main/java/app/eeui/framework/ui/component/webView/WebView.java
@@ -8,6 +8,7 @@ import androidx.annotation.NonNull;
 import android.os.Build;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.webkit.WebSettings;
 
 import com.alibaba.fastjson.JSONObject;
@@ -229,6 +230,10 @@ public class WebView extends WXVContainer<ViewGroup> {
                 }
                 return true;
 
+            case "fullscreen":
+                setFullscreen(eeuiParse.parseBool(val, false));
+                return true;
+
             default:
                 return false;
         }
@@ -332,6 +337,43 @@ public class WebView extends WXVContainer<ViewGroup> {
             v_webview.setScrollContainer(var);
             v_webview.setVerticalScrollBarEnabled(var);
             v_webview.setHorizontalScrollBarEnabled(var);
+        }
+    }
+
+    /**
+     * 设置是否全屏显示
+     * @param fullscreen 是否全屏
+     */
+    @JSMethod
+    public void setFullscreen(boolean fullscreen) {
+        if (v_webview != null) {
+            Activity activity = eeuiParse.parseActivity(getContext());
+            if (activity != null) {
+                if (fullscreen) {
+                    // 设置全屏
+                    // 隐藏状态栏
+                    activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                    // 隐藏导航栏并使内容延伸到导航栏区域
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                        activity.getWindow().getDecorView().setSystemUiVisibility(
+                                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                                View.SYSTEM_UI_FLAG_FULLSCREEN |
+                                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+                    }
+                } else {
+                    // 取消全屏
+                    // 显示状态栏
+                    activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                    // 恢复正常UI显示
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                        activity.getWindow().getDecorView().setSystemUiVisibility(
+                                View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+                    }
+                }
+            }
         }
     }
 
